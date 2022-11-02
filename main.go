a536a5c50ecdf1b545c0fd2742d6aa07833dc2a4
rexim <reximkut@gmail.com>
2022-11-02 23:32:03 +0700

    Fix indentation

--- main.go
+++ main.go
@@ -26,54 +26,55 @@ func isMemberTrusted(member *discordgo.Member) bool {
 func lookupEnvOrDie(name string) string {
 	env, found := os.LookupEnv(name)
 	if !found {
-        log.Fatalln("Could not find ", name, " variable")
+		log.Fatalln("Could not find ", name, " variable")
 	}
-    return env
+	return env
 }
 
 func handleDiscordMessage(db *sql.DB, dg *discordgo.Session, m *discordgo.MessageCreate) {
-    if m.Author.Bot {
-        return
-    }
+	if m.Author.Bot {
+		return
+	}
 
-    if strings.HasPrefix(m.Content, CommandPrefix+"trust ") {
-        if isMemberTrusted(m.Member) {
-            for _, mention := range m.Mentions {
-                mentionMember, err := dg.GuildMember(m.GuildID, mention.ID)
-                if err != nil {
-                    log.Printf("Could not get roles of user %s: %s\n", mention.ID, err)
-                    continue
-                }
+	if strings.HasPrefix(m.Content, CommandPrefix+"trust ") {
+		if isMemberTrusted(m.Member) {
+			for _, mention := range m.Mentions {
+				mentionMember, err := dg.GuildMember(m.GuildID, mention.ID)
+				if err != nil {
+					log.Printf("Could not get roles of user %s: %s\n", mention.ID, err)
+					continue
+				}
 
-                if !isMemberTrusted(mentionMember) {
-                    // TODO: do all of that in a transation that is rollbacked when GuildMemberRoleAdd fails
-                    _, err = db.Exec("INSERT INTO TrustLog (trusterId, trusteeId) VALUES ($1, $2);", m.Author.ID, mention.ID)
-                    if err != nil {
-                        log.Printf("Could not save a TrustLog entry\n");
-                        continue
-                    }
+				if !isMemberTrusted(mentionMember) {
+					// TODO: do all of that in a transation that is rollbacked when GuildMemberRoleAdd fails
+					// TODO: limit the amount of trusts a user can do
+					_, err = db.Exec("INSERT INTO TrustLog (trusterId, trusteeId) VALUES ($1, $2);", m.Author.ID, mention.ID)
+					if err != nil {
+						log.Printf("Could not save a TrustLog entry\n");
+						continue
+					}
 
-                    err := dg.GuildMemberRoleAdd(m.GuildID, mention.ID, TrustedRoleId)
-                    if err != nil {
-                        log.Printf("Could not assign role %s to user %s: %s\n", TrustedRoleId, mention.ID, err)
-                        continue
-                    }
+					err := dg.GuildMemberRoleAdd(m.GuildID, mention.ID, TrustedRoleId)
+					if err != nil {
+						log.Printf("Could not assign role %s to user %s: %s\n", TrustedRoleId, mention.ID, err)
+						continue
+					}
 
-                    dg.ChannelMessageSend(m.ChannelID, "Trusted "+mention.Username)
-                }
-            }
-        } else {
-            log.Printf("User %s is not trusted to trust others\n", m.Member.Nick);
-        }
-    } else {
-        log.Printf("%s is not a trust command", m.Content)
-    }
+					dg.ChannelMessageSend(m.ChannelID, "Trusted "+mention.Username)
+				}
+			}
+		} else {
+			log.Printf("User %s is not trusted to trust others\n", m.Member.Nick);
+		}
+	} else {
+		log.Printf("%s is not a trust command", m.Content)
+	}
 }
 
 func main() {
 	discordToken := lookupEnvOrDie("TREE1984_DISCORD_TOKEN")
-    // TODO: use PostgreSQL url here
-    // postgresql://[user[:password]@][netloc][:port][/dbname][?param1=value1&...]
+	// TODO: use PostgreSQL url here
+	// postgresql://[user[:password]@][netloc][:port][/dbname][?param1=value1&...]
 	pgsqlConnection := lookupEnvOrDie("TREE1984_PGSQL_CONNECTION")
 
 	dg, err := discordgo.New("Bot " + discordToken)
@@ -81,18 +82,22 @@ func main() {
 		log.Fatalln("Could not start a new Discord session:", err)
 	}
 
+	// TODO: REST API for controling/monitoring the bot
+	// TODO: web client for the REST API
+
+	// TODO: migrate database
 	db, err := sql.Open("postgres", pgsqlConnection)
 	if err != nil {
-        log.Fatalln("Could not open PostgreSQL connection:", err)
+		log.Fatalln("Could not open PostgreSQL connection:", err)
 	}
-    err = db.Ping()
-    if err != nil {
-        log.Fatalln("PostgreSQL connection didn't respond to ping:", err)
-    }
+	err = db.Ping()
+	if err != nil {
+		log.Fatalln("PostgreSQL connection didn't respond to ping:", err)
+	}
 
 	dg.AddHandler(func(s *discordgo.Session, m *discordgo.MessageCreate){
-        handleDiscordMessage(db, s, m)
-    })
+		handleDiscordMessage(db, s, m)
+	})
 
 	dg.Identify.Intents = discordgo.IntentsGuildMessages
 
@@ -107,7 +112,7 @@ func main() {
 	signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM, os.Interrupt, os.Kill)
 	<-sc
 
-	// Cleanly close down the Discord session.
-    db.Close()
+	// Cleanly close down the Discord session and the Postgres connection.
+	db.Close()
 	dg.Close()
 }
