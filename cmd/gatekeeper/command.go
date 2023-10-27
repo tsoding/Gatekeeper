2d5cf83b33fd64903671d14fa4f34550dfd3101c
rexim <reximkut@gmail.com>
2023-10-27 18:50:10 +0700

    Add urlencode function

--- cmd/gatekeeper/command.go
+++ cmd/gatekeeper/command.go
@@ -175,6 +175,26 @@ func EvalContextFromCommandEnvironment(env CommandEnvironment, command Command) 
 						}
 						return Expr{}, nil
 					},
+					"urlencode": func(context *EvalContext, args []Expr) (Expr, error) {
+						sb := strings.Builder{}
+						for _, arg := range args {
+							result, err := context.EvalExpr(arg)
+							if err != nil {
+								return Expr{}, err
+							}
+
+							switch result.Type {
+							case ExprVoid:
+							case ExprInt:
+								sb.WriteString(strconv.Itoa(result.AsInt))
+							case ExprStr:
+								sb.WriteString(result.AsStr);
+							default:
+								return Expr{}, fmt.Errorf("%s evaluated into %s which is neither Int, Str, nor Void. `urlencode` command cannot display that.", arg.String(), result.String());
+							}
+						}
+						return NewExprStr(url.PathEscape(sb.String())), nil
+					},
 					"say": func(context *EvalContext, args []Expr) (Expr, error) {
 						sb := strings.Builder{}
 						for _, arg := range args {
