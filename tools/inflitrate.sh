2bb296a89d2548c52f9aee409726c5f2ed32c0e0
rexim <reximkut@gmail.com>
2024-12-21 08:28:16 +0700

    [inflitrate] setup environment variables

--- tools/inflitrate.sh
+++ tools/inflitrate.sh
@@ -2,6 +2,11 @@
 
 set -e
 
+export PGVER=17.2
+export GATEKEEPER_PREFIX="$HOME/Gatekeeper"
+export PGDATA="$GATEKEEPER_PREFIX/data/db" # NOTE(rexim): Tells PostgreSQL where the database is
+export PATH="$GATEKEEPER_PREFIX/pkg/go/bin:$GATEKEEPER_PREFIX/pkg/postgresql-$PGVER/bin/:$PATH"
+
 infiltrate_init() {
     echo "##################################################################"
     echo "# WARNING! This script is a part of an on going effort to create #"
@@ -13,9 +18,9 @@ infiltrate_init() {
     echo "Press Enter to continue or ^C to cancel..."
     read # TODO(rexim): this read is skipped when you `curl https://url/to/inflitrate.sh | bash` it
 
-    # NOTE(rexim): Rough Layout of $HOME/Gatekeeper
+    # NOTE(rexim): Rough Layout of $GATEKEEPER_PREFIX
     #
-    # `-$HOME/Gatekeeper/
+    # `-$GATEKEEPER_PREFIX/
     #   |
     #   `-src/                # Source code
     #   | `-postgres/...
@@ -25,11 +30,10 @@ infiltrate_init() {
     #   | `-gatekeeper/...
     #   | `-go/...
     #   `-data/               # Applications data
-    #     `-postgres/...
-    #     `-gatekeeper/...
-    mkdir -vp "$HOME/Gatekeeper/src"
-    mkdir -vp "$HOME/Gatekeeper/pkg"
-    mkdir -vp "$HOME/Gatekeeper/data"
+    #     `-db/...
+    mkdir -vp "$GATEKEEPER_PREFIX/src"
+    mkdir -vp "$GATEKEEPER_PREFIX/pkg"
+    mkdir -vp "$GATEKEEPER_PREFIX/data"
 
     setup_deps
     setup_postgres
@@ -39,6 +43,7 @@ infiltrate_init() {
 
 setup_deps() {
     . /etc/os-release
+    # TODO(rexim): test on different distro via Docker
     case $ID in
         "void")
             sudo xbps-install -y git bison flex readline readline-devel
@@ -55,42 +60,45 @@ setup_deps() {
 }
 
 setup_postgres() {
-    if [ -e "$HOME/Gatekeeper/pkg/postgresql-17.2/" ]; then
+    if [ -e "$GATEKEEPER_PREFIX/pkg/postgresql-$PGVER/" ]; then
         echo "PostgreSQL is already setup"
         return
     fi
 
-    cd "$HOME/Gatekeeper/src"
+    cd "$GATEKEEPER_PREFIX/src"
 
-    wget https://ftp.postgresql.org/pub/source/v17.2/postgresql-17.2.tar.gz
-    tar fvx postgresql-17.2.tar.gz
-    cd ./postgresql-17.2/
-    ./configure --prefix="$HOME/Gatekeeper/pkg/postgresql-17.2/"
+    wget https://ftp.postgresql.org/pub/source/v$PGVER/postgresql-$PGVER.tar.gz
+    tar fvx postgresql-$PGVER.tar.gz
+    cd ./postgresql-$PGVER/
+    ./configure --prefix="$GATEKEEPER_PREFIX/pkg/postgresql-$PGVER/"
     make -j$(nproc)
     make install
 
-    "$HOME/Gatekeeper/pkg/postgresql-17.2/bin/pg_ctl" init -D "$HOME/Gatekeeper/data/db"
-    # TODO(rexim): create "gatekeeper" user and database
+    initdb -U postgres
+    pg_ctl start
+    createuser gatekeeper -U postgres
+    createdb gatekeeper -U postgres -O gatekeeper
+    pg_ctl stop
 }
 
 setup_go() {
-    if [ -e "$HOME/Gatekeeper/pkg/go/" ]; then
+    if [ -e "$GATEKEEPER_PREFIX/pkg/go/" ]; then
         echo "Go is already setup"
         return
     fi
 
-    cd "$HOME/Gatekeeper/pkg"
+    cd "$GATEKEEPER_PREFIX/pkg"
     wget https://go.dev/dl/go1.23.4.linux-amd64.tar.gz
     tar fvx go1.23.4.linux-amd64.tar.gz
 }
 
 setup_gatekeeper() {
-    if [ -e "$HOME/Gatekeeper/pkg/gatekeeper" ]; then
+    if [ -e "$GATEKEEPER_PREFIX/pkg/gatekeeper" ]; then
         echo "Gatekeeper is already setup"
         return
     fi
 
-    cd "$HOME/Gatekeeper/src"
+    cd "$GATEKEEPER_PREFIX/src"
 
     # TODO(rexim): iirc Go has its own sort of standardized layout of installing packages.
     # It has something to do with $GOPATH and $GOROOT or whatever (I'm not a Go dev, I don't know)
@@ -98,39 +106,39 @@ setup_gatekeeper() {
 
     git clone https://github.com/tsoding/gatekeeper
     cd gatekeeper
-    "$HOME/Gatekeeper/pkg/go/bin/go" build ./cmd/gatekeeper
-    cp -v "$HOME/Gatekeeper/src/gatekeeper/gatekeeper" "$HOME/Gatekeeper/pkg/"
+    go build ./cmd/gatekeeper
+    cp -v "$GATEKEEPER_PREFIX/src/gatekeeper/gatekeeper" "$GATEKEEPER_PREFIX/pkg/"
 
     # TODO(rexim): setup Bots credentials.
     # Preferably walk the user throw the entire process of acquiring them for both Twitch and Discord.
 }
 
 # TODO(rexim): some sort of simple sanity check for all non-"init" commands that the environment was "init"-ed
-# For instance, just check that $HOME/Gatekeeper exists.
+# For instance, just check that $GATEKEEPER_PREFIX exists.
 # TODO(rexim): help command that prints all the available subcommands
 case "$1" in
     "" | "init")
         infiltrate_init
         ;;
     "start")
-        # TODO(rexim): redirect PostgreSQL log file somewhere to $HOME/Gatekeeper/data
-        "$HOME/Gatekeeper/pkg/postgresql-17.2/bin/pg_ctl" start -D "$HOME/Gatekeeper/data/db"
+        # TODO(rexim): redirect PostgreSQL log file somewhere to $GATEKEEPER_PREFIX/data
+        pg_ctl start
         # TODO(rexim): start the bot process in daemon mode somehow?
         ;;
     "stop")
-        "$HOME/Gatekeeper/pkg/postgresql-17.2/bin/pg_ctl" stop -D "$HOME/Gatekeeper/data/db"
+        pg_ctl stop
         # TODO(rexim): stop the bot process if it's running as well
         ;;
     "status")
-        "$HOME/Gatekeeper/pkg/postgresql-17.2/bin/pg_ctl" status -D "$HOME/Gatekeeper/data/db"
+        pg_ctl status
         # TODO(rexim): check the status of the bot process if it's running
         ;;
     "update")
-        cd "$HOME/Gatekeeper/src/gatekeeper/"
+        cd "$GATEKEEPER_PREFIX/src/gatekeeper/"
         git fetch --prune origin
         git merge origin/master
-        "$HOME/Gatekeeper/pkg/go/bin/go" build ./cmd/gatekeeper
-        cp -v "$HOME/Gatekeeper/src/gatekeeper/gatekeeper" "$HOME/Gatekeeper/pkg/"
+        go build ./cmd/gatekeeper
+        cp -v "$GATEKEEPER_PREFIX/src/gatekeeper/gatekeeper" "$GATEKEEPER_PREFIX/pkg/"
         ;;
     *)
         echo "ERROR: unknown subcommand '$1'"
@@ -138,12 +146,9 @@ case "$1" in
         ;;
 esac
 
-# TODO(rexim): subcommand that starts/stops the gatekeeper
-#   The whole things, both PostgreSQL and the Bot process
 # TODO(rexim): subcommand to make/restore backups
-# TODO(rexim): subcommand to pull new changes for the Bot executable
 # TODO(rexim): how do you update the inflitrate.sh script itself?
 #   It is expected to be downloaded standalone into $HOME or what not,
 #   but what if it is updated? Maybe part of the "init" should be replacing
-#   this file with a symlink to $HOME/Gatekeeper/src/gatekeeper/tools/inflitrate.sh
+#   this file with a symlink to $GATEKEEPER_PREFIX/src/gatekeeper/tools/inflitrate.sh
 #   so it's always updated?
