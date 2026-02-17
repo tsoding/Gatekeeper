a0912e88b42aa3f035013bcd05601f5063bf25fe
rexim <reximkut@gmail.com>
2026-02-17 20:41:54 +0700

    brok: implement reinforcement

--- internal/grok.go
+++ internal/grok.go
@@ -7,17 +7,13 @@ import (
 	"math"
 )
 
-const (
-	ThresholdCount = 10
-)
-
 func GrokTokenizeMessage(message string) []string {
 	return strings.FieldsFunc(message, func(x rune) bool {
 		return unicode.IsSpace(x) || unicode.IsPunct(x)
 	})
 }
 
-func grokGetTotalCounts(db *sql.DB) (yesCount int64, noCount int64, err error) {
+func GrokGetTotalCounts(db *sql.DB) (yesCount int64, noCount int64, err error) {
 	var yes bool
 	var count int64
 	rows, err := db.Query("select yes, sum(count) from grok group by yes");
@@ -26,6 +22,8 @@ func grokGetTotalCounts(db *sql.DB) (yesCount int64, noCount int64, err error) {
 	}
 	defer rows.Close()
 
+	yesCount = 1
+	noCount  = 1
 	for rows.Next() {
 		err := rows.Scan(&yes, &count)
 		if err != nil {
@@ -40,7 +38,7 @@ func grokGetTotalCounts(db *sql.DB) (yesCount int64, noCount int64, err error) {
 	return
 }
 
-func grokGetWordsCounts(db *sql.DB, word string) (yesCount int64, noCount int64, err error) {
+func GrokGetWordsCounts(db *sql.DB, word string) (yesCount int64, noCount int64, err error) {
 	var yes bool
 	var count int64
 	rows, err := db.Query("select yes, count from grok where word = $1", word)
@@ -49,6 +47,8 @@ func grokGetWordsCounts(db *sql.DB, word string) (yesCount int64, noCount int64,
 	}
 	defer rows.Close()
 
+	yesCount = 1
+	noCount  = 1
 	for rows.Next() {
 		err := rows.Scan(&yes, &count)
 		if err != nil {
@@ -63,8 +63,42 @@ func grokGetWordsCounts(db *sql.DB, word string) (yesCount int64, noCount int64,
 	return
 }
 
+func GrokMakeSureAllWordsOfPromptExist(db *sql.DB, prompt string) error {
+	for _, token := range GrokTokenizeMessage(prompt) {
+		_, err := db.Exec(`INSERT INTO Grok (yes, word, count)
+	VALUES (true, $1, 1), (false, $1, 1)
+	ON CONFLICT (yes, word) DO NOTHING;`, strings.ToUpper(token))
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func GrokReinforce(db *sql.DB, prompt string, yes bool) error {
+	err := GrokMakeSureAllWordsOfPromptExist(db, prompt)
+	if err != nil {
+		return err
+	}
+
+	for _, token := range GrokTokenizeMessage(prompt) {
+		_, err := db.Exec(`INSERT INTO Grok (yes, word, count)
+	VALUES ($1, $2, 1)
+	ON CONFLICT (yes, word) DO UPDATE SET count = Grok.count + 1;`, yes, strings.ToUpper(token))
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
 func GrokQuery(db *sql.DB, prompt string) (float64, float64, error) {
-	totalYesCount, totalNoCount, err := grokGetTotalCounts(db)
+	err := GrokMakeSureAllWordsOfPromptExist(db, prompt)
+	if err != nil {
+		return 0, 0, err
+	}
+
+	totalYesCount, totalNoCount, err := GrokGetTotalCounts(db)
 	if err != nil {
 		return 0, 0, err
 	}
@@ -77,13 +111,10 @@ func GrokQuery(db *sql.DB, prompt string) (float64, float64, error) {
 	noP    := math.Log(float64(totalNoCount)/float64(totalTotalCount))
 	for _, token := range GrokTokenizeMessage(prompt) {
 		word := strings.ToUpper(token)
-		yesCount, noCount, err := grokGetWordsCounts(db, word)
+		yesCount, noCount, err := GrokGetWordsCounts(db, word)
 		totalCount := yesCount + noCount
 		if err != nil {
 			return 0, 0, err
-		}
-		if totalCount < ThresholdCount {
-			continue
 		}
 		if yesCount != 0 {
 			yesDp += math.Log(float64(yesCount)/float64(totalYesCount))
